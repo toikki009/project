12f5e4b72f8b4cd230413c123bc7f2c4ef984939
toikki009 <irish009@naver.com>
2024-04-10 14:15:55 +0900

    bash_operation

--- dags/dags_email_operator.py
+++ dags/dags_email_operator.py
@@ -11,7 +11,7 @@ with DAG(
 ) as dag:
     send_email_task = EmailOperator(
         task_id='send_email_task',
-        to='irish009@naver.com',
+        to='toikki009@gmail.com',
         subject='Airflow 성공메일',
         html_content='Airflow 작업이 완료되었습니다'
     )
