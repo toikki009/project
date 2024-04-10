1c13938f6027ca307e0f267da0b8885b2c39c2fa
toikki009 <irish009@naver.com>
2024-04-10 16:35:52 +0900

    111

--- dags/dags_push_operator.py
+++ dags/dags_push_operator.py
@@ -12,7 +12,7 @@ with DAG(
 ) as dag:
      bash_t1 = BashOperator(
         task_id="bash_t1",
-        bash_command="echo whoami",
+        bash_command="echo who am i",
     )     
      
      bash_t2 = BashOperator(
