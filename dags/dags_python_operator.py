f535eec753a284474d78d7c47b00c8ba71a36afb
toikki009 <irish009@naver.com>
2024-04-11 10:34:14 +0900

    11

--- dags/dags_python_operator.py
+++ dags/dags_python_operator.py
@@ -15,7 +15,7 @@ with DAG(
         rand_int = random.randint(0,3)
         print(frute[rand_int])
 
-    py_t1 = PythonOperator(
+    py_t1 = PythonOperator( 
         task_id = 'py_t1',
         python_callable=select_fruit
     )
